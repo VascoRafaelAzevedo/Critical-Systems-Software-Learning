2ef49aad994cee332993f5c3a52dc3621b34ebf6
Vasco Rafael Azevedo <1230776@isep.ipp.pt>
2025-10-27 12:15:22 +0100

    more cpp learning

---
+++ 01_C_Language/learning_C++/0000-LIVRO_PPP/014-try_compile_7/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>                          // we find the declaration of cout in here
+
+int f(int);                              // declaration of f
+
+int main()
+{
+         int i = 7;                      // declaration of i
+         std::cout << f(i) << '\n';
+}
+
+//link command failes 
---
+++ 01_C_Language/learning_C++/0000-LIVRO_PPP/015-try_compile_7_2/main.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+
+struct X
+{
+    void f(int x)
+    {
+        struct Y
+        {
+            int f() { return 1; }
+            int m;
+        };
+        int m;
+        m = x;
+        Y m2;
+        return f(m2.f());
+    }
+    int m;
+    void g(int m)
+    {
+        if (0 < m)
+            f(m + 2);
+        else
+        {
+            g(m + 2.3);
+        }
+    }
+    X() {}
+    int m3()
+    {
+        return 0;
+    }
+
+    void main()
+    {
+        X a;
+        a.f(2);
+    }
+};
